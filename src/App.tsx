be31a8591d4cb1f26b63462166cde3f807990cc6
Josian van Efferen <github@Josian.nl>
2023-04-24 14:53:34 +0200

    blackjack v1

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,9 @@
 import { useState } from 'react'
 import './index.css'
-import { BrowserRouter as Router, Routes, Route } from
-  'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Topbar from './components/Topbar'
 import Bottombar from './components/Bottombar'
+import BlackjackScreen from './components/BlackjackScreen';
 
 function App() {
   const [count, setCount] = useState(0)
@@ -12,7 +12,7 @@ function App() {
     <Router>
       <Topbar />
       <Routes>
-        {/* <Route path='/' exact element={<Home />} /> */}
+        {<Route path='/' element={<BlackjackScreen />} /> }
         {/* <Route path='/startup' exact element={<Home />} /> */}
       </Routes>
       <Bottombar />
