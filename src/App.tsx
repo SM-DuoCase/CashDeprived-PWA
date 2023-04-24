9c2b9d4c3ce73d43e27bf72f8993cc30251b11b9
Niels <Nielsfeijen76@gmail.com>
2023-04-24 10:10:59 +0200

    nav tester

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,23 @@
 import { useState } from 'react'
 import './index.css'
-import Topbar from './components/topbar'
+import { BrowserRouter as Router, Routes, Route } from
+  'react-router-dom';
+import Topbar from './components/Topbar'
 import Bottombar from './components/Bottombar'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <div>
+    <Router>
       <Topbar />
+      <Routes>
+        {/* <Route path='/' exact element={<Home />} /> */}
+        {/* <Route path='/startup' exact element={<Home />} /> */}
+      </Routes>
       <Bottombar />
-    </div>
+    </Router>
+
   )
 }
 
