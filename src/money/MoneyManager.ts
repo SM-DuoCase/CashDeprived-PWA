c2cf27194e862b53f21e4b90532f1491f19e0af9
Josian van Efferen <github@Josian.nl>
2023-05-10 10:23:08 +0200

    Added better moneymanager

--- src/money/MoneyManager.ts
+++ src/money/MoneyManager.ts
@@ -1,22 +1,79 @@
-export let money: number =  getMoney()
-
-function getMoney():number {
-    const _money = localStorage.getItem("money")
-    if (_money != null) {
-        return parseInt(_money)
-    } else {
-        localStorage.setItem("money", "0")
-        return 0
+type Player = {
+    name: string,
+    balance: number,
+    totalLost: number,
+    lostToday: number,
+    blackjack: {
+        lostMoney: number,
+        lostTimes: number
+    },
+    roulette: {
+        lostMoney: number,
+        lostTimes: number
+    },
+    slotsmachine: {
+        lostMoney: number,
+        lostTimes: number
+    },
+    poker: {
+        lostMoney: number,
+        lostTimes: number
     }
 }
 
 
+let PlayerStats: Player = {
+    name: "Niels Feijen",
+    balance: 25.00,
+    totalLost: 0.00,
+    lostToday: 0.00,
+    blackjack: {
+        lostMoney: 0.00,
+        lostTimes: 0
+    },
+    roulette: {
+        lostMoney: 0.00,
+        lostTimes: 0
+    },
+    slotsmachine: {
+        lostMoney: 0.00,
+        lostTimes: 0
+    },
+    poker: {
+        lostMoney: 0.00,
+        lostTimes: 0
+    }
+}
+
+function getPlayerObject(): Player {
+    const _player = localStorage.getItem("PlayerStats")
+    if (_player != null) {
+        const player:Player = JSON.parse(_player)
+        return player
+    } else {
+        localStorage.setItem("PlayerStats", JSON.stringify(PlayerStats));
+        return PlayerStats
+    }
+}
+
+function savePlayerObject(playerObject: Player) {
+    localStorage.setItem("PlayerStats", JSON.stringify(playerObject));
+}
+
+export function getMoney():number {
+    const player = getPlayerObject()
+    return player.balance
+}
+
+
 export function addMoney(amount:number) {
-    money += amount;
-    localStorage.setItem("money", money.toString())
+    const player:Player = getPlayerObject()
+    player.balance += amount;
+    savePlayerObject(player)
 }
 
 export function removeMoney(amount:number) {
-    money -= amount;
-    localStorage.setItem("money", money.toString())
+    const player:Player = getPlayerObject()
+    player.balance -= amount;
+    savePlayerObject(player)
 }
